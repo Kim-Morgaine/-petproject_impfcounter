fc4474026c5fbda2a6f667f4c20bd9962df79d8b
Kim-Morgaine <75619359+Kim-Morgaine@users.noreply.github.com>
2021-01-09 21:33:04 +0100

    new graph

--- visualisation.py
+++ visualisation.py
@@ -12,21 +12,20 @@ st.title("Vaccination covid 19 counter for switzerland")
 
 st.sidebar.title("Lookup your Kanton")
 st.sidebar.markdown('Interact with the data here')
-option = st.sidebar.selectbox(
-    'Kantons ',
-     ['Zürich', 'Zug'])
+
 
 @st.cache
 def read_data():
     impf = pd.read_excel("impfkategorien.xlsx", skiprows=2)
-    impf_ = impf.groupby("Priorität").sum()
-    impf_plt = pd.DataFrame(impf_["Grösse der Zielgruppe schweizweit laut BAG-Schätzung"]).transpose()
-    impf_plt["AnzahlImpfungen"] = 0
-    impf_plt = pd.DataFrame(np.array([[0,0,0,0,0,1000000]]), columns=['P1', 'P2', 'P3', 'P4', 'Sonstige', 'AnzahlImpfungen']).append(impf_plt, ignore_index=True)
-    data = impf_plt.rename(index={0: "Anzahl Impfungen",1: "Anzahl Personen"})
-    return data
+    return impf
 
-data = read_data()
+data_ = read_data()
+
+impf_ = data_.groupby("Priorität").sum()
+impf_plt = pd.DataFrame(impf_["Grösse der Zielgruppe schweizweit laut BAG-Schätzung"]).transpose()
+impf_plt["AnzahlImpfungen"] = 0
+impf_plt = pd.DataFrame(np.array([[0,0,0,0,0,1000000]]), columns=['P1', 'P2', 'P3', 'P4', 'Sonstige', 'AnzahlImpfungen']).append(impf_plt, ignore_index=True)
+data = impf_plt.rename(index={0: "Anzahl Impfungen",1: "Anzahl Personen"})
 
 import datetime
 today = st.date_input("Today is", datetime.datetime.now())
@@ -43,4 +42,28 @@ ax = data.plot(kind='barh', stacked=True, color= pal)
 ax.set_xticklabels(mio)
 st.pyplot()
 
+
+
+st.markdown('Distribution of the risk patients')
+df = impf_.drop(columns= ["Grösse der Zielgruppe schweizweit laut BAG-Schätzung"])
+bars =('P1', 'P2', 'P3', 'P4', 'Sonstige')
+y_pos = np.arange(len(bars))
+plt.bar(y_pos,list(df["Bern"]/df["Bern"].sum()))
+plt.xticks(y_pos, bars)
+st.pyplot()
+
+option = st.sidebar.selectbox(
+    'Priority Groups',
+     ['P1', 'P2', 'P3', 'P4', 'Sonstige'])
+
+if option == "P1":
+    st.table(data_["Beschreibung"].loc[data_['Priorität'] == "P1"])
+if option == "P2":
+    st.table(data_["Beschreibung"].loc[data_['Priorität'] == "P2"])
+if option == "P3":
+    st.table(data_["Beschreibung"].loc[data_['Priorität'] == "P3"])
+if option == "P4":
+    st.table(data_["Beschreibung"].loc[data_['Priorität'] == "P4"])
+if option == "Sonstige":
+    st.table(data_["Beschreibung"].loc[data_['Priorität'] == "Sonstige"])
 
